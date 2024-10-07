54b6d04f0132213f27bda9ffb59e64046fced539
usuiat <stripewani@gmail.com>
2024-10-08 07:41:27 +0900

    HelloCompose

---
+++ Ch1_Sec1_1_HelloCompose/app/src/main/java/net/engawapg/composebooksamples/ch1_sec1_1_hellocompose/MainActivity.kt
@@ -0,0 +1,50 @@
+package net.engawapg.composebooksamples.ch1_sec1_1_hellocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.engawapg.composebooksamples.ch1_sec1_1_hellocompose.ui.theme.Ch1_Sec1_1_HelloComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ch1_Sec1_1_HelloComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        HelloCompose()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloCompose() {
+    Column {
+        var isVisible by remember { mutableStateOf(false) }
+        Button(onClick = { isVisible = true }) {
+            Text("Button")
+        }
+        if (isVisible) {
+            Text("Hello")
+        }
+    }
+}
