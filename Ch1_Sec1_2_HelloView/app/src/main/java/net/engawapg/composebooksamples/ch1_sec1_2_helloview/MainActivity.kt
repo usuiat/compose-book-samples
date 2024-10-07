4d1800d8d556bd7a713d444bd36627bffe4b6908
usuiat <stripewani@gmail.com>
2024-10-08 07:50:09 +0900

    HelloView

---
+++ Ch1_Sec1_2_HelloView/app/src/main/java/net/engawapg/composebooksamples/ch1_sec1_2_helloview/MainActivity.kt
@@ -0,0 +1,32 @@
+package net.engawapg.composebooksamples.ch1_sec1_2_helloview
+
+import android.os.Bundle
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            textView.visibility = VISIBLE
+        }
+    }
+}
