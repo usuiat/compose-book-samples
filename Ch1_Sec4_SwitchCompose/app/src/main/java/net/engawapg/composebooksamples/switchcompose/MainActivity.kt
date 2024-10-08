0c6b4f433c59da73b9db9828ffb10b75ae6a6604
usuiat <stripewani@gmail.com>
2024-10-08 22:14:29 +0900

    SwitchCompose

---
+++ Ch1_Sec4_SwitchCompose/app/src/main/java/net/engawapg/composebooksamples/switchcompose/MainActivity.kt
@@ -0,0 +1,50 @@
+package net.engawapg.composebooksamples.switchcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.engawapg.composebooksamples.switchcompose.ui.theme.Ch1_Sec4_SwitchComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ch1_Sec4_SwitchComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(10.dp),
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        Text("Off")
+                        SwitchSample()
+                        Text("On")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SwitchSample() {
+    var checked by remember { mutableStateOf(false) }
+    Switch(checked = checked, onCheckedChange = { checked = !checked })
+}
