e890aa02095d49e671c334e1c0e3b1e603396366
usuiat <stripewani@gmail.com>
2024-10-10 22:18:42 +0900

    Coroutinesのサンプルを追加

---
+++ Ch5_ComposeMechanismSample/app/src/main/java/net/engawapg/composebooksamples/composemechanismsample/Sec5_Coroutines.kt
@@ -0,0 +1,33 @@
+package net.engawapg.composebooksamples.composemechanismsample
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.EmptyCoroutineContext
+
+fun main() {
+    val scope = CoroutineScope(EmptyCoroutineContext)
+    scope.launch {
+        println("start")
+        delay(1000)
+        println("end")
+    }
+
+    // コルーチンの処理が終わるのを待つ
+    Thread.sleep(2000L)
+
+    scope.launch {
+        println("Start 1")
+        delay(1000)
+        println("End 1")
+    }
+    scope.launch {
+        println("Start 2")
+        delay(1000)
+        println("End 2")
+    }
+    scope.cancel()
+
+    Thread.sleep(2000L)
+}
