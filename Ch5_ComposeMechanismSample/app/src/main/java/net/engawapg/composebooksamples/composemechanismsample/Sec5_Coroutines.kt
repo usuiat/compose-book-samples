98092c17486fa42e78a85f214eccae45dac886a3
usuiat <stripewani@gmail.com>
2024-10-20 15:55:53 +0900

    コルーチンのサンプルを更新した

--- Ch5_ComposeMechanismSample/app/src/main/java/net/engawapg/composebooksamples/composemechanismsample/Sec5_Coroutines.kt
+++ Ch5_ComposeMechanismSample/app/src/main/java/net/engawapg/composebooksamples/composemechanismsample/Sec5_Coroutines.kt
@@ -8,14 +8,18 @@ import kotlin.coroutines.EmptyCoroutineContext
 
 fun main() {
     val scope = CoroutineScope(EmptyCoroutineContext)
+
+    println("launchでコルーチンを起動する例")
+
     scope.launch {
         println("start")
         delay(1000)
         println("end")
     }
 
-    // コルーチンの処理が終わるのを待つ
-    Thread.sleep(2000L)
+    Thread.sleep(2000L) // コルーチンの処理が終わるのを待つ
+
+    println("複数のコルーチンを起動する例")
 
     scope.launch {
         println("Start 1")
@@ -24,10 +28,25 @@ fun main() {
     }
     scope.launch {
         println("Start 2")
+        delay(500)
+        println("End 2")
+    }
+
+    Thread.sleep(2000L) // コルーチンの処理が終わるのを待つ
+
+    println("コルーチンスコープをキャンセルする例")
+
+    scope.launch {
+        println("Start 1")
         delay(1000)
+        println("End 1")
+    }
+    scope.launch {
+        println("Start 2")
+        delay(500)
         println("End 2")
     }
     scope.cancel()
 
-    Thread.sleep(2000L)
+    Thread.sleep(2000L) // コルーチンの処理が終わるのを待つ
 }
