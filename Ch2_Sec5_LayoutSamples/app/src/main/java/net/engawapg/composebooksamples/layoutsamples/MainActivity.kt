23920913e7ac7d47c052cd753f9157d9a9f1cd43
usuiat <stripewani@gmail.com>
2024-11-05 23:10:01 +0900

    コメントアウトしているサンプルコードで必要なimportを記述しておく

--- Ch2_Sec5_LayoutSamples/app/src/main/java/net/engawapg/composebooksamples/layoutsamples/MainActivity.kt
+++ Ch2_Sec5_LayoutSamples/app/src/main/java/net/engawapg/composebooksamples/layoutsamples/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import net.engawapg.composebooksamples.layoutsamples.ui.theme.LayoutSamplesTheme
 
