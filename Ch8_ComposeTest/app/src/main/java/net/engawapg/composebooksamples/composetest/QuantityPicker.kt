179021c04d37fb5cb9b0449598eb9fb1c8bcb55a
usuiat <stripewani@gmail.com>
2024-11-07 22:19:15 +0900

    不要なimportを削除

--- Ch8_ComposeTest/app/src/main/java/net/engawapg/composebooksamples/composetest/QuantityPicker.kt
+++ Ch8_ComposeTest/app/src/main/java/net/engawapg/composebooksamples/composetest/QuantityPicker.kt
@@ -1,25 +1,15 @@
 package net.engawapg.composebooksamples.composetest
 
-import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import net.engawapg.composebooksamples.composetest.ui.theme.ComposeTestTheme
 
 @Composable
 fun QuantityPicker(state: QuantityPickerState, modifier: Modifier = Modifier) {
